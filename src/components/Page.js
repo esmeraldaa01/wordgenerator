8dd4b68644a95a37c4c19dca6991f4bf1e804b5f
esmeraldaa01 <lameesmeralda7@gmail.com>
2022-03-31 15:40:29 +0200

    first commit

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import styled from "styled-components";
+import styled , {css} from "styled-components";
 
 const Center = styled.div`
   position: absolute;
@@ -19,39 +19,63 @@ const Choice  = styled.p`
   border-radius: 50px;
   padding: 10px;
   background-color: orange;
+  ${props => props.active && css`
+    background: blueviolet;
+  `}
 `;
 const Title  = styled.p`
   font-weight: bold;
   color: #07074d;
 `;
 
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 const Page = () => {
     const [text , setText] = useState('');
-    const [showAnimation ,setShowAnimation] = useState(false)
-    const [currentIndex, setCurrentIndex] = useState()
+    const ref = React.useRef({})
+
+    const [activeIndex, setActiveIndex] = React.useState(null);
 
 
     let str = text.split(/[,]/);
     if(str.length >= 1)  str.pop()
-    let random =  str[Math.floor(Math.random() * str.length)];
+    // let random =  str[Math.floor(Math.random() * str.length)];
 
 
-        setInterval(() => {
-            for(let j = 0; j < 4; j++) {
-                setTimeout(() => {
-                    for (let i = 0; i < str.length; i++) {
-                        setCurrentIndex(i);
-                    }
-                    return () => clearTimeout();
-                }, 100);
-                return () => clearTimeout();
-                setCurrentIndex(2)
-            }
-            return () => clearInterval();
-            setCurrentIndex(2)
-        }, 5000)
+        // setInterval(() => {
+        //     for(let j = 0; j < 4; j++) {
+        //         setTimeout(() => {
+        //             for (let i = 0; i < str.length; i++) {
+        //                 setCurrentIndex(i);
+        //             }
+        //             return () => clearTimeout();
+        //         }, 100);
+        //         return () => clearTimeout();
+        //         setCurrentIndex(2)
+        //     }
+        //     return () => clearInterval();
+        //     setCurrentIndex(2);
+        // }, 5000)
 
-    // Math.floor(Math.random())
+    const changeDirection = (index = 0, count) => {
+        if (count > ref.current.maxCount) return
+
+        if (index === str.length) index = 0;
+
+        setTimeout(() => {
+            setActiveIndex(index);
+            changeDirection(index + 1, count + 1);
+        }, 200);
+    }
+
+    const handleStartAnimation = () => {
+        ref.current.maxCount = getRandomNumber(1, 10);
+        changeDirection(0, activeIndex)
+    }
+
 
     const handleChange = (e) => {
         if(e.target.value.endsWith(',')){
@@ -63,11 +87,11 @@ const handleEnter = (e) => {
     const textarea = document.getElementById('output');
         if(e.key === 'Enter'){
             textarea.value = '';
-            setShowAnimation(true);
+            handleStartAnimation()
         }
 }
 
-console.log(currentIndex , 'c')
+
     return (
         <Center>
             <Title>Insert your words , seperated with comma !</Title>
@@ -75,7 +99,7 @@ console.log(currentIndex , 'c')
            <Choices>{str.map((x, index) => {
          if(x === "") return null
                return (
-              <Choice key={index} style={{ backgroundColor:  (index === currentIndex || showAnimation && (currentIndex === x))  ? 'blue': ''}}>{x}</Choice>
+              <Choice active={index === activeIndex} key={index}>{x}</Choice>
                    )
            })}</Choices>
         </Center>
